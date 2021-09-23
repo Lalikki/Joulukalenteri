06cd981a39d6d25543a06297f1e62a3cf0904601
Lalikki <laurijarvinen@hotmail.com>
2021-09-23 21:45:12 +0300

    image swap working

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,7 +3,11 @@ import { calendarContent } from '../utils/CalendarContent';
 
 const Calendar = () => {
 
-    const [selectedCard, setSelectedCard] = useState(calendarContent[0]);
+    // const [selectedCard, setSelectedCard] = useState(calendarContent[0]);
+
+    // function swapImage(e) {
+    //     e.target.setAttribute('src', calendarContent.contentImage)
+    // }
 
     return (
         <div className="calendar-wrapper">
@@ -18,26 +22,28 @@ const Calendar = () => {
                         <div key={index} className="col-sm-3 mb-3">
 
                             <div id="content-card" className="card d-flex">
-                                <img src={image} alt="" id="content-image" className="card-img-top" />
+                                <img src={image} onClick={(e) => {
+                                    e.target.setAttribute('src', contentImage)
+                                }} id="content-image" className="card-img-top" />
 
                                 <div className="card-body d-flex justify-content-center">
                                     <h5 className="card-title">{title}</h5>
                                 </div>
                                 <ul className="list-group list-group-flush">
                                     <li className="list-group-item">
-
                                         <p className="card-text content-paragraph">{description}</p>
+                                    </li>
+                                    <li className="list-group-item">
+                                        <p className="card-text content-paragraph">Timer tähän?</p>
                                     </li>
                                 </ul>
 
-                                <div className="card-body d-flex justify-content-center">
-
+                                {/* <div className="card-body d-flex justify-content-center">
                                     <a href="#" onClick={() => {
-                                        setSelectedCard(selectedCard)
-                                        document.getElementById('content-image').src = selectedCard.contentImage
-
+                                        // document.getElementById('content-image').src = selectedCard.contentImage
                                     }} className="btn btn-primary">Open</a>
-                                </div>
+                                </div> */}
+
                             </div>
                         </div>
                     ))}

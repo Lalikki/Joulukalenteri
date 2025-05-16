f42a53cbdaf561f9e89519554cf193004d9842b1
Lalikki <laurijarvinen@hotmail.com>
2025-05-17 02:48:19 +0300

    uutta

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Calendar from './components/Calendar';
